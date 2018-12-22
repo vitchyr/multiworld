55ae52aa5288e0b9bea2990731d75e081760bca8
Soroush Nasiriany <snasiriany@gmail.com>
2018-12-21 18:12:08 -0800

    made some critical changes to wheeled_car

--- multiworld/core/image_env.py
+++ multiworld/core/image_env.py
@@ -76,7 +76,7 @@ class ImageEnv(ProxyEnv, MultitaskEnv):
             # viewer = mujoco_py.MjRenderContextOffscreen(sim, device_id=-1)
             # init_camera(viewer.cam)
             # sim.add_render_context(viewer)
-        self._render_local = False
+        self._render_local = True #False
         img_space = Box(0, 1, (self.image_length,), dtype=np.float32)
         self._img_goal = img_space.sample() #has to be done for presampling
         spaces = self.wrapped_env.observation_space.spaces.copy()
