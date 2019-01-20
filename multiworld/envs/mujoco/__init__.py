8674f3de5fad84634ecb7704c34aa18c99c4eb44
Steven Lin <stevenlin1111@gmail.com>
2019-01-20 15:02:00 -0800

    Add registered envs

--- multiworld/envs/mujoco/__init__.py
+++ multiworld/envs/mujoco/__init__.py
@@ -434,6 +434,60 @@ def register_custom_envs():
 
     )
 
+    """
+    ICML Envs
+    """
+    register(
+        id='SawyerPushNIPS-v1',
+        entry_point='multiworld.envs.mujoco.sawyer_xyz'
+                    '.sawyer_push_nips:SawyerPushAndReachXYEasyEnv',
+        tags={
+            'git-commit-hash': 'b9413652d02c0aca35f2b548ca01429d05b320ea',
+            'author': 'steven',
+        },
+        kwargs=dict(
+            hide_goal=True,
+            reward_info=dict(
+                type="state_distance",
+            ),
+        )
+
+    )
+    register(
+        id='SawyerDoorHookResetFreeEnv-v1',
+        entry_point='multiworld.envs.mujoco.sawyer_xyz'
+                    '.sawyer_door_hook:SawyerDoorHookEnv',
+        tags={
+            'git-commit-hash': 'b9413652d02c0aca35f2b548ca01429d05b320ea',
+            'author': 'steven',
+        },
+        kwargs=dict(
+            goal_low=(-0.1, 0.45, 0.1, 0),
+            goal_high=(0.05, 0.65, .25, .83),
+            hand_low=(-0.1, 0.45, 0.1),
+            hand_high=(0.05, 0.65, .25),
+            max_angle=.83,
+            xml_path='sawyer_xyz/sawyer_door_pull_hook.xml',
+            reward_type='angle_diff_and_hand_distance',
+            reset_free=True,
+        )
+    )
+
+    register(
+        id='SawyerReachXYZEnv-v2',
+        entry_point='multiworld.envs.mujoco.sawyer_xyz.sawyer_reach:SawyerReachXYZEnv',
+        tags={
+            'git-commit-hash': 'b9413652d02c0aca35f2b548ca01429d05b320ea',
+            'author': 'steven'
+        },
+        kwargs={
+            'hide_goal_markers': True,
+            'norm_order': 2,
+        },
+    )
+
+
+
 
 
 
