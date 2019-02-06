cfd401705c2bf2b353dc544b969327bc7c0e49db
Murtaza Dalal <mdalal@berkeley.edu>
2019-02-06 15:07:27 -0800

    fixed

--- multiworld/envs/real_world/sawyer/sawyer_door.py
+++ multiworld/envs/real_world/sawyer/sawyer_door.py
@@ -79,7 +79,7 @@ class SawyerDoorEnv(sawyer_door.SawyerDoorEnv, MultitaskEnv):
         if self.use_dynamixel:
             super()._reset_robot_and_door()
         else:
-            super()._reset_robot()
+            super()._reset_robot_and_door()
         goal = self.sample_goal()
         self._state_goal = goal['state_desired_goal']
         return self._get_obs()
