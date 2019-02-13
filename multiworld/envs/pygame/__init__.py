999440c962072932af1f867632fe18a75b69fa4d
Vitchyr Pong <vitchyr@gmail.com>
2019-02-12 17:15:48 -0800

    add in Soroush's envs
    finish adding envs from soroush
    
    add easy

--- multiworld/envs/pygame/__init__.py
+++ multiworld/envs/pygame/__init__.py
@@ -41,6 +41,57 @@ def register_custom_envs():
             'render_onscreen': True,
         },
     )
+    register(
+        id='Point2D-Box-Wall-v1',
+        entry_point='multiworld.envs.pygame.point2d:Point2DWallEnv',
+        tags={
+            'git-commit-hash': '73c8823',
+            'author': 'vitchyr'
+        },
+        kwargs={
+            'action_scale': 1.,
+            'wall_shape': 'box',
+            'wall_thickness': 2.0,
+            'render_size': 84,
+            'render_onscreen': True,
+            'images_are_rgb': True,
+            'render_target': True,
+        },
+    )
+    register(
+        id='Point2D-Big-UWall-v1',
+        entry_point='multiworld.envs.pygame.point2d:Point2DWallEnv',
+        tags={
+            'git-commit-hash': '73c8823',
+            'author': 'vitchyr'
+        },
+        kwargs={
+            'action_scale': 0.25,
+            'wall_shape': 'big-u',
+            'wall_thickness': 0.50,
+            'render_size': 84,
+            'images_are_rgb': True,
+            'render_onscreen': True,
+            'render_target': True,
+        },
+    )
+    register(
+        id='Point2D-Easy-UWall-v1',
+        entry_point='multiworld.envs.pygame.point2d:Point2DWallEnv',
+        tags={
+            'git-commit-hash': '73c8823',
+            'author': 'vitchyr'
+        },
+        kwargs={
+            'action_scale': 0.25,
+            'wall_shape': 'easy-u',
+            'wall_thickness': 0.50,
+            'render_size': 84,
+            'images_are_rgb': True,
+            'render_onscreen': True,
+            'render_target': True,
+        },
+    )
 
 
 register_custom_envs()
