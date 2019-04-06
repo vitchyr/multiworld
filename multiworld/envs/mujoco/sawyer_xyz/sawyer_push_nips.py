980facdfd16d9836ac38c92a3bf5b06111e6b0b4
Vitchyr Pong <vitchyr@gmail.com>
2019-04-06 00:37:57 -0700

    implement get diagnostics to be forward-compatible (#76)
    * fix set goal
    
    * implement get_diagnostics
    
    * Remove log_diagnostics

--- multiworld/envs/mujoco/sawyer_xyz/sawyer_push_nips.py
+++ multiworld/envs/mujoco/sawyer_xyz/sawyer_push_nips.py
@@ -371,10 +371,7 @@ class SawyerPushAndReachXYEnv(MujocoEnv, Serializable, MultitaskEnv):
                 1, 0, 1, 0,
                 ]
 
-    def log_diagnostics(self, paths, logger=None, prefix=""):
-        if logger is None:
-            return
-
+    def get_diagnostics(self, paths, prefix=""):
         statistics = OrderedDict()
         for stat_name in [
             'hand_distance',
@@ -385,18 +382,16 @@ class SawyerPushAndReachXYEnv(MujocoEnv, Serializable, MultitaskEnv):
             stat_name = stat_name
             stat = get_stat_in_paths(paths, 'env_infos', stat_name)
             statistics.update(create_stats_ordered_dict(
-                '%s %s' % (prefix, stat_name),
+                '%s%s' % (prefix, stat_name),
                 stat,
                 always_show_all_stats=True,
             ))
             statistics.update(create_stats_ordered_dict(
-                'Final %s %s' % (prefix, stat_name),
+                'Final %s%s' % (prefix, stat_name),
                 [s[-1] for s in stat],
                 always_show_all_stats=True,
             ))
-
-        for key, value in statistics.items():
-            logger.record_tabular(key, value)
+        return statistics
 
     """
     Multitask functions
