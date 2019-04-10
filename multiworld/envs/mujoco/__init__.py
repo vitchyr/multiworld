e1813f1c94782b21cc2bdac4eaf2c47323224e92
Murtaza Dalal <mdalal@berkeley.edu>
2019-04-10 09:21:38 -0700

    updated commit hash

--- multiworld/envs/mujoco/__init__.py
+++ multiworld/envs/mujoco/__init__.py
@@ -63,7 +63,7 @@ def register_custom_envs():
         id='SawyerReachTorqueEnv-v0',
         entry_point='multiworld.envs.mujoco.sawyer_torque.sawyer_reach_torque_env:SawyerReachTorqueEnv',
         tags={
-            'git-commit-hash': 'dd10a1f',
+            'git-commit-hash': '27b2112',
             'author': 'murtaza'
         },
         kwargs={
@@ -76,7 +76,7 @@ def register_custom_envs():
         id='SawyerReachTorqueGripperEnv-v0',
         entry_point='multiworld.envs.mujoco.sawyer_torque.sawyer_torque_reacher_with_gripper:SawyerReachTorqueGripperEnv',
         tags={
-            'git-commit-hash': 'dd10a1f',
+            'git-commit-hash': '27b2112',
             'author': 'murtaza'
         },
         kwargs={
