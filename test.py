0601fa230265de4dd3d86b3c7a3d98f438c492e1
Steven Lin <stevenlin598@berkeley.edu>
2019-05-08 11:09:17 -0700

    add oracle sampling with wall

--- test.py
+++ test.py
@@ -7,19 +7,20 @@ from multiworld.envs.mujoco.cameras import sawyer_pick_and_place_camera
 import numpy as np
 
 env = SawyerPickAndPlaceEnvYZ(
-    hand_low=(-0.1, 0.55, 0.05),
-    hand_high=(0.0, 0.65, 0.15),
-    action_scale=0.02,
+    hand_low=(-0.1, 0.45, 0.05),
+    hand_high=(0.0, 0.75, 0.15),
+    action_scale=0.03,
     hide_goal_markers=True,
     num_goals_presampled=50,
-    p_obj_in_hand=1,
+    oracle_reset_prob=1.0,
+    random_init=True,
 )
 
 while True:
     env.reset()
-    env.set_to_goal(
-        {'state_desired_goal': env.generate_uncorrected_env_goals(1)['state_desired_goal'][0]}
-    )
+    # env.set_to_goal(
+        # {'state_desired_goal': env.generate_uncorrected_env_goals(1)['state_desired_goal'][0]}
+    # )
     # action = np.array([0, 0, 1])
     # for _ in range(20):
         # env.step(action)
@@ -37,12 +38,11 @@ while True:
         # env.step(action)
         # env.render()
 
-    for _ in range(100):
-        # action = np.array([0, 0, 1])
-        action = env.action_space.sample()
-        for _ in range(10):
-            obs, _, _, _ = env.step(action)
-            # print(env.get_obj_pos())
-            env.render()
-        # obs, _, _, _ = image_env.step(np.zeros(3))
-        # render_cv2(obs)
+    action = env.action_space.sample()
+    for _ in range(20):
+        obs, _, _, _ = env.step(action)
+        # print(env.get_obj_pos())
+        env.render()
+        print(env.get_obj_pos())
+    # obs, _, _, _ = image_env.step(np.zeros(3))
+    # render_cv2(obs)
