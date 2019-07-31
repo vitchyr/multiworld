26a777b3787074ccd8598fa7f08c2cd376888e8a
Vitchyr Pong <vitchyr@gmail.com>
2019-07-31 11:52:08 -0700

    Fix qpos space

--- multiworld/envs/mujoco/classic_mujoco/ant.py
+++ multiworld/envs/mujoco/classic_mujoco/ant.py
@@ -87,6 +87,7 @@ class AntEnv(MujocoEnv, Serializable, MultitaskEnv, metaclass=abc.ABCMeta):
         else:
             self.obs_space = Box(obs_space_low, obs_space_high, dtype=np.float32)
             self.goal_space = Box(goal_space_low, goal_space_high, dtype=np.float32)
+        qpos_space = Box(-10*np.ones(15), 10*np.ones(15))
 
         spaces = [
             ('observation', self.obs_space),
@@ -104,8 +105,8 @@ class AntEnv(MujocoEnv, Serializable, MultitaskEnv, metaclass=abc.ABCMeta):
             ]
         if self.goal_is_qpos:
             spaces += [
-                ('qpos_desired_goal', self.goal_space),
-                ('qpos_achieved_goal', self.goal_space),
+                ('qpos_desired_goal', qpos_space),
+                ('qpos_achieved_goal', qpos_space),
             ]
 
         self.observation_space = Dict(spaces)
