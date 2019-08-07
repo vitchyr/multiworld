2eac9a886716a88c5a552015f4d05bfa7195cdc9
Soroush Nasiriany <snasiriany@gmail.com>
2019-08-07 16:27:11 -0700

    update logging information

--- multiworld/envs/env_util.py
+++ multiworld/envs/env_util.py
@@ -46,7 +46,18 @@ def create_stats_ordered_dict(
             and not always_show_all_stats):
         return OrderedDict({name: float(data)})
 
+    if len(data)//10 == 0:
+        mean_trimmed = np.mean(np.sort(data))
+    else:
+        mean_trimmed = np.mean(np.sort(data)[len(data)//10:-len(data)//10])
+
     stats = OrderedDict([
+        (name + ' p10', np.percentile(data, 10)),
+        (name + ' p90', np.percentile(data, 90)),
+        (name + ' p25', np.percentile(data, 25)),
+        (name + ' p75', np.percentile(data, 75)),
+        (name + ' MeanTrimmed', mean_trimmed),
+        (name + ' Med', np.median(data)),
         (name + ' Mean', np.mean(data)),
         (name + ' Std', np.std(data)),
     ])
