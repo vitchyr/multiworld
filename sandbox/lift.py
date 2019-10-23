0a5270a27d04d215b1453eb5de50869cb8e535b0
Michael Janner <janner@berkeley.edu>
2019-10-22 17:23:25 -0700

    minimal sawyer lift example

---
+++ sandbox/lift.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pdb
+
+import roboverse
+import roboverse.devices as devices
+
+space_mouse = devices.SpaceMouse()
+space_mouse.start_control()
+
+env = roboverse.make('SawyerLift-v0', render=True, gripper_bounds=[0,1])
+
+while True:
+	env.step(space_mouse.control, space_mouse.control_gripper)
