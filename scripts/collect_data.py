470ee465243109319f76ecf2f8c2e3a063b89fc1
Michael Janner <janner@berkeley.edu>
2019-11-02 00:56:03 -0700

    record min grasp step in data collection

--- scripts/collect_data.py
+++ scripts/collect_data.py
@@ -8,7 +8,7 @@ parser.add_argument('--env', type=str, default='SawyerLift-v0')
 parser.add_argument('--savepath', type=str, default='data/fast-joints-grasping/')
 parser.add_argument('--gui', type=rv.utils.str2bool, default=None)
 parser.add_argument('--render', type=rv.utils.str2bool, default=None)
-parser.add_argument('--horizon', type=int, default=500)
+parser.add_argument('--horizon', type=int, default=250)
 parser.add_argument('--num_episodes', type=int, default=200)
 args = parser.parse_args()
 
@@ -22,11 +22,12 @@ print('Observation space: {} | Action space: {}'.format(env.observation_space, e
 for ep in range(args.num_episodes):
 	obs = env.reset()
 	ep_rew = 0
+	min_grasp_step = None
 	images = []
 	for i in range(args.horizon):
 		act = policy.get_action(obs)
-		if act[-1] > .5:
-			print(i)
+		if act[-1] > 0 and min_grasp_step is None:
+			min_grasp_step = i
 		next_obs, rew, term, info = env.step(act)
 		pool.add_sample(obs, act, next_obs, rew, term)
 		obs = next_obs
@@ -40,7 +41,7 @@ for ep in range(args.num_episodes):
 
 		if term: break
 
-	print(ep, i+1, ep_rew)
+	print('Episode: {} | steps: {} | return: {} | min grasp step: {}'.format(ep, i+1, ep_rew, min_grasp_step))
 	if args.render:
 		rv.utils.save_video('{}/{}.avi'.format(args.savepath, ep), images)
 
