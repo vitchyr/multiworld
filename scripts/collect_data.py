f4e8e6af491b5cc3969acf0ba6343f73be1fdd4f
janner <janner@mit.edu>
2019-11-09 04:01:47 -0800

    collecting reward bonus data

--- scripts/collect_data.py
+++ scripts/collect_data.py
@@ -5,7 +5,7 @@ import pdb
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--env', type=str, default='SawyerLift-v0')
-parser.add_argument('--savepath', type=str, default='data/mult4-scale2-rep10-step1-lift/')
+parser.add_argument('--savepath', type=str, default='data/mult4-bonus1-scale2-rep20-step1-lift/')
 parser.add_argument('--gui', type=rv.utils.str2bool, default=None)
 parser.add_argument('--render', type=rv.utils.str2bool, default=None)
 parser.add_argument('--horizon', type=int, default=100)
@@ -17,7 +17,7 @@ rv.utils.make_dir(args.savepath)
 ## 2 / 10 / 2 : 1, 1.25, 1.5, 1.75, 2
 ## 1 / 4 / 2 : 1.5, 2
 
-env = rv.make(args.env, goal_mult=4, action_scale=.2, action_repeat=10, timestep=1./120, gui=args.gui,)
+env = rv.make(args.env, goal_mult=4, action_scale=.2, action_repeat=20, timestep=1./120, gui=args.gui,)
 policy = rv.policies.GraspingPolicy(env, env._sawyer, env._cube)
 pool = rv.utils.DemoPool()
 print('Observation space: {} | Action space: {}'.format(env.observation_space, env.action_space))
@@ -36,7 +36,7 @@ for ep in range(args.num_episodes):
 		pool.add_sample(obs, act, next_obs, rew, term)
 		obs = next_obs
 
-		print(i, rew, term)
+		# print(i, rew, term)
 		ep_rew += rew
 		if args.render:
 			img = env.render()
