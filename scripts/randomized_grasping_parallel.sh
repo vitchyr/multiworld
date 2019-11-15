62efa9f12f62460c367c2f7a6b350387c35935fe
Jonathan Yang <jydumplings@gmail.com>
2019-11-15 15:12:54 -0800

    Change parameter for parallel grasping data collection

--- scripts/randomized_grasping_parallel.sh
+++ scripts/randomized_grasping_parallel.sh
@@ -1,7 +1,7 @@
 for i in {1..20}
 do
 	python3 randomized_scripted_grasping.py --video_save_directory videos --data_save_directory trajectories --num_trajectories 100 &
-	sleep 1
+	sleep 2
 done
 wait
 
