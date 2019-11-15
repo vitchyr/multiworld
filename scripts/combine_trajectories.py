a25a411d1eabb5c4d3af3ebc594b0e61678072ab
Jonathan Yang <jydumplings@gmail.com>
2019-11-15 15:01:04 -0800

    Allow storing separate trajectories in pool.

--- scripts/combine_trajectories.py
+++ scripts/combine_trajectories.py
@@ -11,25 +11,20 @@ args = parser.parse_args()
 args.data_directory = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'data', 'SawyerGrasp', args.data_directory)
 args.output_directory = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'data', 'SawyerGrasp', args.output_directory)
 
-keys = ('observations', 'actions', 'next_observations', 'rewards', 'terminals')
-fields = {}
+trajectories = []
 timestamp = roboverse.utils.timestamp()
-
-for key in keys:
-    fields[key] = []
 
 for root, dirs, files in os.walk(args.data_directory):
     for f in files:
         if "pool" in f:
             with open(os.path.join(args.data_directory, f), 'rb') as fp:
-                trajectories = pickle.load(fp)
+                t = pickle.load(fp)
 
-            for key in keys:
-                fields[key].extend(trajectories[key])
+            trajectories.extend(t)
 
 if not os.path.exists(args.output_directory):
     os.makedirs(args.output_directory)
 
-print(fields[observations])
+print(trajectories[0])
 with open(os.path.join(args.output_directory, 'data_{0}.pkl'.format(timestamp)), 'wb+') as fp:
-    pickle.dump(fields, fp)
+    pickle.dump(trajectories, fp)
