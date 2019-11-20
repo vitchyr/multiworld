Fix grasping env so that object angle is in quaternions

shapenet_scripts/scripted_grasping.py
@@ -13,15 +13,15 @@ obj_key = 'lego'
 num_grasps = 0
 
 env.reset()
-#target_pos += np.random.uniform(low=-0.05, high=0.05, size=(3,))
+# target_pos += np.random.uniform(low=-0.05, high=0.05, size=(3,))
 images = []
 target_pos = env.get_object_midpoint(obj_key)
 
 target_pos[0] += -0.02
 target_pos[1] += 0.00
+target_pos[2] += -0.03
 
 for i in range(50):
-
     ee_pos = env.get_end_effector_pos()
 
     if i < 25:
@@ -43,16 +43,20 @@ for i in range(50):
         action[2] = 1.0
         grip=1.
 
+    action = np.append(action, [grip])
+
     if args.save_video:
         img = env.render()
         images.append(img)
 
     time.sleep(0.05)
-    env.step(action, grip)
+    o, r, d, info = env.step(action)
+    # print(action)
+    # print(o)
 
 object_pos = env.get_object_midpoint(obj_key)
 if object_pos[2] > -0.1:
     num_grasps += 1
 
 if args.save_video:
-    utils.save_video('dump/grasp_duck_single/{}.avi'.format(0), images)
+    utils.save_video('data/lego_test_{}.avi'.format(0), images)