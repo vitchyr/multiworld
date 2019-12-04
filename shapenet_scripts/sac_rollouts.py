4e2b6e28af47d6f2a6ada047c18b2af1e4872098
Avi Singh <avisingh599@gmail.com>
2019-12-03 22:34:18 -0800

    bugfix in sac_rollouts

--- shapenet_scripts/sac_rollouts.py
+++ shapenet_scripts/sac_rollouts.py
@@ -75,7 +75,7 @@ def simulate_policy(args):
                                np.random.normal(scale=args.noise_std)
                 next_state, reward, done, info = \
                     evaluation_environment.step(action_noisy)
-                pool.add_sample(observation, action, next_state, reward, done)
+                pool.add_sample(observation, action_noisy, next_state, reward, done)
                 observation = next_state
                 if args.gui:
                     time.sleep(0.1)
