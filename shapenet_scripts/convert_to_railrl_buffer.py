ca3b2d66ec2c250c71cbe5bd174e859b12e1dbb5
Avi Singh <avisingh599@gmail.com>
2020-04-06 18:27:59 -0700

    Add a couple of scripts for converting buffer formats

---
+++ shapenet_scripts/convert_to_railrl_buffer.py
@@ -0,0 +1,70 @@
+import pickle
+import os.path as osp
+import numpy as np
+from tqdm import tqdm
+
+from railrl.data_management.obs_dict_replay_buffer import \
+    ObsDictReplayBuffer
+import roboverse
+
+# Buffer for scripted reaching from pixels
+# INPUT_BUFFER = ('/media/avi/data/Work/github/jannerm/bullet-manipulation/data/'
+#                 'feb26_SawyerReach-v0_pixels_2K_dense_reward_randomize_noise'
+#                 '_std_0.3/combined_all_2020-02-26T18-52-21.pkl')
+# OUTPUT_NAME = 'railrl_obs_dict_buffer_100K.pkl'
+
+INPUT_BUFFER = ('/media/avi/data/Work/github/jannerm/bullet-manipulation/data/'
+                'sac_runs_feb11/env-11Feb2020-af888d54-seed='
+                '527_2020-02-11_20-11-04uk2zf5y4/pixels_buffer/'
+                'consolidated_buffer_pixel_only.pkl')
+OUTPUT_NAME = 'railrl_obs_dict_grasping_buffer_1M.pkl'
+
+
+if __name__ == "__main__":
+    input_buffer = pickle.load(open(INPUT_BUFFER, 'rb'))
+    output_dir = osp.dirname(INPUT_BUFFER)
+    output_filename = osp.join(output_dir, OUTPUT_NAME)
+
+    input_buffer_size = len(input_buffer['actions'])
+    env = roboverse.make('SawyerReach-v0', gui=False, randomize=True,
+                         observation_mode='pixels', reward_type='shaped',
+                         transpose_image=True)
+    output_buffer = ObsDictReplayBuffer(input_buffer_size, env, observation_key='image')
+
+    path_length = 50
+    assert input_buffer_size % path_length == 0
+    num_traj = int(input_buffer_size/path_length)
+
+    for i in tqdm(range(num_traj)):
+        start_index = i*path_length
+        end_index = i*path_length + path_length
+        path = dict(
+            actions=np.asarray(input_buffer['actions'][start_index:end_index]),
+            rewards=np.asarray(input_buffer['rewards'][start_index:end_index]),
+            terminals=np.asarray(input_buffer['terminals'][start_index:end_index]),
+        )
+
+        obs_processed = []
+        next_obs_processed = []
+        for j in range(start_index, end_index):
+            image = np.transpose(input_buffer['observations'][j][0]['image'],
+                                 (2, 0, 1))
+            image = image.flatten()
+            image = np.float32(image)/255.0
+            state = input_buffer['observations'][j][0]['state']
+            obs_processed.append(dict(image=image, state=state))
+
+            next_image = np.transpose(input_buffer['next_observations'][j][0]['image'],
+                                 (2, 0, 1))
+            next_image = next_image.flatten()
+            next_image = np.float32(next_image) / 255.0
+            next_state = input_buffer['next_observations'][j][0]['state']
+            next_obs_processed.append(dict(image=next_image, state=next_state))
+
+        path['observations'] = obs_processed
+        path['next_observations'] = next_obs_processed
+
+        output_buffer.add_path(path)
+
+    input_buffer = None
+    pickle.dump(output_buffer, open(output_filename, 'wb'), protocol=4)
---
+++ shapenet_scripts/convert_to_railrl_buffer_state.py
@@ -0,0 +1,53 @@
+import pickle
+import os.path as osp
+import numpy as np
+from tqdm import tqdm
+
+from railrl.data_management.env_replay_buffer import EnvReplayBuffer
+import roboverse
+
+
+# INPUT_BUFFER = ('/media/avi/data/Work/github/jannerm/bullet-manipulation/data/'
+#                 'feb25_SawyerReach-v0_state_2K_dense_reward_randomize_noise_std'
+#                 '_0.3/combined_all_2020-02-25T11-52-48.pkl')
+
+INPUT_BUFFER = ('/media/avi/data/Work/github/jannerm/bullet-manipulation/data/'
+                'sac_runs_feb11/env-11Feb2020-af888d54-seed=527_2020-02-11_20-'
+                '11-04uk2zf5y4/consolidated_buffer.pkl')
+
+OUTPUT_NAME = 'railrl_obs_dict_buffer_100K.pkl'
+
+
+if __name__ == "__main__":
+    input_buffer = pickle.load(open(INPUT_BUFFER, 'rb'))
+    output_dir = osp.dirname(INPUT_BUFFER)
+    output_filename = osp.join(output_dir, OUTPUT_NAME)
+
+    input_buffer_size = len(input_buffer['actions'])
+    env = roboverse.make('SawyerReach-v0', gui=False, randomize=True,
+                         observation_mode='state', reward_type='shaped',
+                         transpose_image=True)
+    output_buffer = EnvReplayBuffer(input_buffer_size, env)
+
+    path_length = 50
+    assert input_buffer_size % path_length == 0
+    num_traj = int(input_buffer_size/path_length)
+
+    for i in tqdm(range(num_traj)):
+        start_index = i*path_length
+        end_index = i*path_length + path_length
+        path = dict(
+            actions=np.asarray(input_buffer['actions'][start_index:end_index]),
+            rewards=np.asarray(input_buffer['rewards'][start_index:end_index]),
+            terminals=np.asarray(
+                input_buffer['terminals'][start_index:end_index]),
+            observations=np.asarray(
+                input_buffer['observations'][start_index:end_index]),
+            next_observations=np.asarray(
+                input_buffer['next_observations'][start_index:end_index]),
+            agent_infos=np.asarray([{} for i in range(path_length)]),
+            env_infos=np.asarray([{} for i in range(path_length)]),
+        )
+        output_buffer.add_path(path)
+    input_buffer = None
+    pickle.dump(output_buffer, open(output_filename, 'wb'))
