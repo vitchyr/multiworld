c68a3b6c640da1e6bc3be73aa8e520a2c8a12909
Avi Singh <avisingh599@gmail.com>
2020-04-25 22:28:14 -0700

    Register some more envs

--- roboverse/envs/registration.py
+++ roboverse/envs/registration.py
@@ -109,6 +109,28 @@ SEQUENTIAL_ENVIRONMENT_SPECS = (
     },
 )
 
+GRASP_V3_ENV_SPECS = []
+OBJ_IDS_TEN = [0, 1, 25, 30, 50, 215, 255, 265, 300, 310]
+for i, obj_id in enumerate(OBJ_IDS_TEN):
+    env_params = dict(
+        id='SawyerGraspOne-{}-V3-v0'.format(i),
+        entry_point=('roboverse.envs.sawyer_grasp_v3:SawyerGraspV3Env'),
+        kwargs={'max_force': 100,
+                   'action_scale': 0.05,
+                   'pos_init': [0.7, 0.2, -0.2],
+                   'pos_low': [.5, -.05, -.38],
+                   'pos_high': [.9, .30, -.15],
+                   'object_position_low': (.65, .10, -.20),
+                   'object_position_high': (.80, .25, -.20),
+                   'num_objects': 1,
+                   'height_threshold': -0.3,
+                   'object_ids': [obj_id]
+               }
+    )
+    GRASP_V3_ENV_SPECS.append(env_params)
+GRASP_V3_ENV_SPECS = tuple(GRASP_V3_ENV_SPECS)
+
+
 PROJECTION_ENVIRONMENT_SPECS = tuple(
     {
         'id': env['id'].split('-')[0] + '2d-' + env['id'].split('-')[1],
@@ -125,7 +147,10 @@ PARALLEL_ENVIRONMENT_SPECS = tuple(
     } for env in SEQUENTIAL_ENVIRONMENT_SPECS + PROJECTION_ENVIRONMENT_SPECS
 )
 
-BULLET_ENVIRONMENT_SPECS = SEQUENTIAL_ENVIRONMENT_SPECS + PROJECTION_ENVIRONMENT_SPECS + PARALLEL_ENVIRONMENT_SPECS
+BULLET_ENVIRONMENT_SPECS = SEQUENTIAL_ENVIRONMENT_SPECS + \
+                           PROJECTION_ENVIRONMENT_SPECS + \
+                           PARALLEL_ENVIRONMENT_SPECS + \
+                           GRASP_V3_ENV_SPECS
 
 def register_bullet_environments():
     for bullet_environment in BULLET_ENVIRONMENT_SPECS:
