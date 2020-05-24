9bd137ce1b43cee209b0908d9584b8b9bf751075
Vitchyr Pong <vitchyr@gmail.com>
2020-05-23 22:56:42 -0700

    add flat wall env and backwards compatibility

--- multiworld/envs/pygame/point2d.py
+++ multiworld/envs/pygame/point2d.py
@@ -73,6 +73,7 @@ class Point2DEnv(MultitaskEnv, Serializable):
         self.pointmass_color = pointmass_color
         self.bg_color = bg_color
         self._wall_color = wall_color
+        self.render_drawer = None
 
         self.max_target_distance = self.boundary_dist - self.target_radius
 
@@ -548,6 +549,9 @@ class Point2DEnv(MultitaskEnv, Serializable):
         pass
 
 
+Point2D = Point2DEnv  # for backwards compatibility
+
+
 class Point2DWallEnv(Point2DEnv):
     """Point2D with walls"""
 
@@ -604,6 +608,16 @@ class Point2DWallEnv(Point2DEnv):
                     -self.inner_wall_max_dist,
                     self.inner_wall_max_dist,
                 )
+            ]
+        if wall_shape == "---":
+            self.walls = [
+                HorizontalWall(
+                    self.ball_radius,
+                    0,
+                    -self.inner_wall_max_dist*2,
+                    self.inner_wall_max_dist*2,
+                    self.wall_thickness
+                ),
             ]
         if wall_shape == "big-u":
             self.walls = [
